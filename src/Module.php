0113568883d811116227a2926826935a297f8fdf
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-04-02 22:24:30 -0700

    Initial commit

---
+++ src/Module.php
@@ -0,0 +1,25 @@
+<?php
+namespace LeoGalleguillos\reCAPTCHA;
+
+class Module
+{
+    public function getConfig()
+    {
+        return [
+            'view_helpers' => [
+                'aliases' => [
+                ],
+                'factories' => [
+                ],
+            ],
+        ];
+    }
+
+    public function getServiceConfig()
+    {
+        return [
+            'factories' => [
+            ],
+        ];
+    }
+}
