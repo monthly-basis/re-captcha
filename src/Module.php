a8fd8d7ff604e5682c2d55eb1fc9fc814a98e5b3
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-04-02 23:30:06 -0700

    Add div helper to helper config

--- src/Module.php
+++ src/Module.php
@@ -10,9 +10,16 @@ class Module
         return [
             'view_helpers' => [
                 'aliases' => [
+                    'getReCaptchaDivTag'    => ReCaptchaHelper\DivTag::class,
                     'getReCaptchaScriptTag' => ReCaptchaHelper\ScriptTag::class,
                 ],
                 'factories' => [
+                    ReCaptchaHelper\DivTag::class => function ($serviceManager) {
+                        $config = $serviceManager->get('Config')['re-captcha'];
+                        return new ReCaptchaHelper\DivTag(
+                            $config['site-key']
+                        );
+                    },
                     ReCaptchaHelper\ScriptTag::class => function ($serviceManager) {
                         return new ReCaptchaHelper\ScriptTag();
                     },
