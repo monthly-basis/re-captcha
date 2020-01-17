c6dec80886721082a92e47a029ad0f1376c6b125
Leo Galleguillos <leo.galleguillos@icloud.com>
2020-01-17 23:40:29 +0000

    Rename variables

--- src/Model/Service/Valid.php
+++ src/Model/Service/Valid.php
@@ -15,19 +15,20 @@ class Valid
             return false;
         }
 
-        $data = [
+        $postData = [
             'secret'   => $this->secretKey,
             'response' => $_POST['g-recaptcha-response'],
             'remoteip' => $_SERVER['REMOTE_ADDR'],
         ];
+
         $curl = curl_init('https://www.google.com/recaptcha/api/siteverify');
         curl_setopt($curl, CURLOPT_POST, true);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $postData);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($curl);
+        $jsonResponseString = curl_exec($curl);
         curl_close($curl);
 
-        $json = json_decode($response);
+        $json = json_decode($jsonResponseString);
         return (bool) $json->success;
     }
 }
